7036201b169f3e9290ec569bf8806829cc991118
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 09:14:35 +0530

    Auto Push Commit

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -4,7 +4,7 @@ resource "aws_spot_instance_request" "rabbitmq" {
   instance_type          = "t3.micro"
   wait_for_fulfillment   = true 
   vpc_security_group_ids = [aws_security_group.allow_rabbitmq.id]
-  subnet_id              = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]
+  subnet_id              = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS[0]
 
   tags = {
     Name = "rabbitmq-${var.ENV}"
